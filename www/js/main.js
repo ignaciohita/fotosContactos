fd0db812f01169c269ca8b0a0b6ed1a1160d1b76
Jose Ignacio Hita Barraza <ignaciohita@gmail.com>
2016-01-02 16:37:52 +0100

    Initial commit

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,5 @@
 var contactoActual;
+var contactoActual;
 
 function seleccionarContacto() {
     "use strict";
@@ -7,6 +8,7 @@ function seleccionarContacto() {
         if (contactoSeleccionado.photos && contactoSeleccionado.photos.length > 0) {
             contactoActual = contactoSeleccionado;
             document.getElementById("fotoContacto").src = contactoSeleccionado.photos[0].value;
+            document.getElementById("botonCambiarFoto").disabled = false;
         } else {
             navigator.notification.alert("El contacto que has seleccionado no tiene foto");
         }
@@ -15,11 +17,26 @@ function seleccionarContacto() {
     });
 }
 
+function cambiarFoto() {
+    "use strict";
+
+    navigator.camera.getPicture(function (imagen) {
+        document.getElementById("fotoContacto").src = imagen;
+        contactoActual.photos[0].value = imagen;
+
+        contactoActual.save();
+    }, function (error) {
+        navigator.notification.alert("Hubo un error al tomar la foto: " + error.code);
+    }, {
+        targetWidth: 720,
+        targetHeight: 720
+    });
+}
+
 function dispositivoListo() {
     "use strict";
 
     document.getElementById("botonSeleccionarContacto").disabled = false;
-
 }
 
 function inicioAplicacion() {
