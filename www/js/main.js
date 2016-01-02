35f2d4a9b985cf2117f9a52d87f50621fa1a43ea
ignaciohita <ignaciohita@gmail.com>
2016-01-02 15:36:31 +0100

    Initial commit

---
+++ www/js/main.js
@@ -0,0 +1,33 @@
+var contactoActual;
+
+function seleccionarContacto() {
+    "use strict";
+
+    navigator.contacts.pickContact(function (contactoSeleccionado) {
+        if (contactoSeleccionado.photos && contactoSeleccionado.photos.length > 0) {
+            contactoActual = contactoSeleccionado;
+            document.getElementById("fotoContacto").innerHTML = contactoSeleccionado.photos[0];
+        } else {
+            navigator.notification.alert("El contacto que has seleccionado no tiene foto");
+        }
+    }, function (errorContacto) {
+        navigator.notification.alert("Hubo un error seleccionando el contacto");
+    });
+}
+
+function dispositivoListo() {
+    "use strict";
+
+    document.getElementById("botonSeleccionarContacto").disabled = false;
+
+}
+
+function inicioAplicacion() {
+    "use strict";
+
+    var i,
+        nodePosition,
+        linkPosition;
+
+    document.addEventListener("deviceready", dispositivoListo);
+}
